1f18e648fb0dfc2cff150147773b1661e705d750
Yinhe Zhang <crickzhang1@gmail.com>
2017-10-20 00:16:05 +0800

    add more items to .gitignore

--- init.el
+++ init.el
@@ -59,7 +59,8 @@
  '(package-selected-packages
    (quote
     (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu monokai-theme adoc-mode markdown-mode)))
- '(save-place t))
+ '(save-place t)
+ '(save-place-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

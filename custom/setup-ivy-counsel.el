fda0ed009cc79692a56ce38158ce44c4495437f0
Yinhe Zhang <crickzhang1@gmail.com>
2017-10-20 10:10:31 +0800

    Add counsel-gtags when using ivy for <24.4

--- custom/setup-ivy-counsel.el
+++ custom/setup-ivy-counsel.el
@@ -22,4 +22,15 @@
   :init
   (counsel-projectile-on))
 
+(use-package counsel-gtags)
+;; Set company for gtags
+(setq company-backends '((company-dabbrev-code company-gtags)))
+;; Set counsel-gtags
+(add-hook 'c-mode-hook 'counsel-gtags-mode)
+(add-hook 'c++-mode-hook 'counsel-gtags-mode)
+(define-key counsel-gtags-mode-map (kbd "M-t") 'counsel-gtags-find-definition)
+(define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
+(define-key counsel-gtags-mode-map (kbd "M-s") 'counsel-gtags-find-symbol)
+(define-key counsel-gtags-mode-map (kbd "M-,") 'counsel-gtags-go-backward)
+
 (provide 'setup-ivy-counsel)

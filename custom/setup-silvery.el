f8921d38ebf5cd91afbad57349c8c0657cc1a84f
Yinhe Zhang <crickzhang1@gmail.com>
2018-07-04 19:19:52 +0800

    fix reset existing file name after saving

--- custom/setup-silvery.el
+++ custom/setup-silvery.el
@@ -1,18 +1,20 @@
 ;;; package --- Summary
-;
-; Convenience functions for myself
+;;
+;; Convenience functions for myself
 
 ;;; Commentary:
 
 ;;; Code:
 
+;; Modifed from https://emacs.stackexchange.com/a/5815
 (defun silvery/copy-buffer-to-file (filename)
   "Save a copy of current buffer to FILENAME!"
-  (interactive "File to write? ")
-  (let ((bufname (buffer-name)))
+  (interactive "FProvide FILENAME to write? ")
+  (let ((bufname (buffer-name))
+        (oldfname (buffer-file-name)))
     (set-visited-file-name filename)
     (save-buffer)
-    (set-visited-file-name nil)
+    (set-visited-file-name oldfname)
     (rename-buffer bufname)))
 
 (provide 'setup-silvery)
